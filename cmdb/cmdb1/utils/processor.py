47540a6410f1d1f9686ebaf0b9e8f3eac8cadeca
cool-guang <519349139@qq.com>
2017-06-27 00:34:21 +0800

    基本前端页面展示完成

---
+++ cmdb/cmdb1/utils/processor.py
@@ -0,0 +1,36 @@
+import pymysql.cursors
+
+
+class processor():
+    @classmethod
+    def loop(self, host, port, user, password):
+        servers = {}
+        try:
+            link = pymysql.connect(host=host, user=user, password=password, port=port,
+                                   charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
+        except Exception as e:
+            servers['Connect_Error'] = str(e)
+            servers['Slave_Healthy'] = 'red'
+            return servers
+        with link.cursor() as cursors:
+            try:
+                sql = "SHOW SLAVE STATUS"
+                rs = cursors.execute(sql)
+                rs = cursors.fetchall()
+            except Exception as e:
+                servers['Query_Error'] = str(e)
+                servers['Slave_Healthy'] = 'red'
+                return servers
+        row = rs[0]
+        if 500 < row['Seconds_Behind_Master'] or 'No' in [row['Slave_IO_Running'], row['Slave_SQL_Running']]:
+            row['Slave_Healthy'] = 3
+        elif 30 < row['Seconds_Behind_Master'] and row['Seconds_Behind_Master'] <= 500:
+            row['Slave_Healthy'] = 2
+        elif 5 < row['Seconds_Behind_Master'] and row['Seconds_Behind_Master'] <= 30:
+            row['Slave_Healthy'] = 1
+        else:
+            row['Slave_Healthy'] = 0
+        row['myHost'] = host
+        row['myPort'] = port
+        servers = row
+        return servers
--- cmdb/cmdb1/views.py
+++ cmdb/cmdb1/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from cmdb1.models import department1 as v1
+from cmdb1.utils.processor import processor
 # Create your views here.
 
 
@@ -8,4 +10,17 @@ def index(request):
 
 
 def department1(request):
-    return render(request, "department1.html")
+    # 首先拿取主库,做个字典雏形
+    sqlStatus = {}
+    masterList = v1.objects.filter(type = 1)
+    for master in masterList:
+        sqlStatus[master.ip+":"+str(master.port)] = {}
+    # 拿取从库开始查询
+    slaveList = v1.objects.filter(type = 2)
+    for slave in slaveList:
+        queryStatus = processor.loop(slave.ip,slave.port,slave.username,slave.password)
+        sqlStatus[queryStatus['Master_Host'] + ":" + str(queryStatus['Master_Port'])][slave.ip +":"+str(slave.port)] = queryStatus
+    masterLen = len(masterList)
+    slaveLen = len(slaveList)
+    msg = "总共 "+str(masterLen)+" 台主库  "+str(slaveLen)+" 台从库."
+    return render(request, "department1.html",{'sqlStatus':sqlStatus,'msg':msg})
